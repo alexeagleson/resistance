42de17adb0988791ef8a65764491225c5d0d428f
Jodie <jodieeagleson@gmail.com>
2020-04-03 22:19:05 -0400

    added movement and tuvix's will to live

--- src/Game.tsx
+++ src/Game.tsx
@@ -9,23 +9,59 @@ const displayConfig: Partial<DisplayOptions> = {
   bg: "grey",
   fg: "pink"
 };
-var display = new ROT.Display(displayConfig);
+
+let y = 4;
+let x = 8;
+const display = new ROT.Display(displayConfig);
 const gameCanvas = display.getContainer();
-display.draw(25, 4, ["V", "T"], null, null);
+display.draw(x, y, ["V", "T"], null, null);
+
+const input = document.createElement("input");
+
+input.addEventListener("keydown", function(e) {
+  const code = e.keyCode;
+  const ch = String.fromCharCode(code);
+  let vk = "?"; /* find the corresponding constant */
+  for (const name in ROT.KEYS) {
+    if ((ROT.KEYS as any)[name] == code && name.indexOf("VK_") == 0) {
+      vk = name;
+    }
+  }
+  display.draw(x, y, " ", null, null);
+  if (vk === "VK_UP") y--;
+  if (vk === "VK_DOWN") y++;
+  if (vk === "VK_RIGHT") x++;
+  if (vk === "VK_LEFT") x--;
+
+  display.draw(x, y, ["V", "T"], null, null);
+});
+
+input.style.width = "1px";
 
 export const Game = () => {
   useEffect(() => {
     const divTag = document.querySelector("#game-wrapper");
     console.log(divTag);
-    if (gameCanvas !== null) divTag?.appendChild(gameCanvas);
+    if (gameCanvas !== null) {
+      divTag?.appendChild(input);
+      divTag?.appendChild(gameCanvas);
+      input.focus();
+    }
   }, []);
   return (
     <>
       <div id="game-wrapper"></div>
       <button
         onClick={() => {
-          display.draw(Math.round(Math.random()*25),Math.round(Math.random()*6), "T", 'red', null)
-        }}>
+          display.draw(
+            Math.round(Math.random() * 25),
+            Math.round(Math.random() * 6),
+            "T",
+            "red",
+            null
+          );
+        }}
+      >
         Spawn Tuvok
       </button>
     </>
