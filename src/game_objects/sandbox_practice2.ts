3383e7a54c95608f1d308ad68948e242e53f4e79
Jodie <jodieeagleson@gmail.com>
2020-04-29 22:19:47 -0400

    end practice 2

--- src/game_objects/sandbox_practice2.ts
+++ src/game_objects/sandbox_practice2.ts
@@ -33,16 +33,22 @@ console.log(arrayOfTenXThree);
 
 // Question 6
 const arrayGreaterHun = arrayOfTen.filter((num) => {
-  if (num >= 100) {return true}
-  return false
+  if (num >= 100) {
+    return true;
+  }
+  return false;
 });
 console.log(arrayGreaterHun);
 
 // Question 7
 const typeTelling = (userInput: any) => {
-  if (typeof userInput === `number`) {return "This is def a number"}
-  else if (typeof userInput === `object`) {return "This is prob an array"}
-  else if (typeof userInput === `string`) {return "Totes a string"}
+  if (typeof userInput === `number`) {
+    return "This is def a number";
+  } else if (typeof userInput === `object`) {
+    return "This is prob an array";
+  } else if (typeof userInput === `string`) {
+    return "Totes a string";
+  }
   return "What the heck is that";
 };
 
@@ -50,3 +56,68 @@ console.log(typeTelling("5"));
 console.log(typeTelling(arrayOfTen));
 console.log(typeTelling(5));
 console.log(typeTelling(typeTelling));
+
+// Question 8
+export interface Product {
+  name: string;
+  department: string;
+  sku: number;
+  ingredients: string[];
+}
+
+// Question 9
+export interface Store {
+  name: string;
+  address: string;
+  city: string;
+  departments: string[];
+  storenum: number;
+  products: Product[];
+}
+
+// Question 10
+const bun: Product = {
+  name: " a hot bag of buns",
+  department: " Bakery",
+  sku: 8003,
+  ingredients: [" flour", " yeast", " salt"],
+};
+
+const corn: Product = {
+  name: " sweet corn",
+  department: " Produce",
+  sku: 8004,
+  ingredients: [" corn"],
+};
+
+const chip: Product = {
+  name: " salty chips",
+  department: " Snack",
+  sku: 8005,
+  ingredients: [" potato", " oil", " more salt"],
+};
+
+const store1: Store = {
+  name: `Bilky Bob\'s`,
+  address: `34 Bob St`,
+  city: `Bilkerton`,
+  departments: ["bakery ", "produce ", "snacks ", "health "],
+  storenum: 44,
+  products: [chip, corn, bun],
+};
+
+const aboutStore = (store: Store) => {
+  return `Welcome to ${store.name}, where everyone in ${
+    store.city
+  } goes to buy food. You will find amazing things like${store.products.map(
+    (product) => {
+      return `${product.name}`;
+    }
+  )} that are teeming with${store.products.map((product) => {
+    return `${product.ingredients} `;
+  })} from the ${store.products.map((product) => {
+        return `${product.department}`;
+      })} departments just as you were destined to do.`;
+};
+
+console.log(aboutStore(store1));
